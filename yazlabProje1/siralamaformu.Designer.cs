0a22b90e29198e80cd5a7169b779733d511f0955
Mehmet Yılmaz <yilmazmehmet7711@gmail.com>
2024-10-20 17:29:20 +0300

    siralama dogru yapiliyor

--- yazlabProje1/siralamaformu.Designer.cs
+++ yazlabProje1/siralamaformu.Designer.cs
@@ -28,33 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.yazlab1_tarifDataSet8 = new yazlabProje1.yazlab1_tarifDataSet8();
+            this.tblTariflerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tbl_TariflerTableAdapter = new yazlabProje1.yazlab1_tarifDataSet8TableAdapters.Tbl_TariflerTableAdapter();
+            this.yazlab1_tarifDataSet9 = new yazlabProje1.yazlab1_tarifDataSet9();
+            this.tblTariflerBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.tbl_TariflerTableAdapter1 = new yazlabProje1.yazlab1_tarifDataSet9TableAdapters.Tbl_TariflerTableAdapter();
+            this.yazlab1_tarifDataSet10 = new yazlabProje1.yazlab1_tarifDataSet10();
+            this.tblTariflerBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.tbl_TariflerTableAdapter2 = new yazlabProje1.yazlab1_tarifDataSet10TableAdapters.Tbl_TariflerTableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tarifAdiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hazirlanmaSuresiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tblTariflerBindingSource3 = new System.Windows.Forms.BindingSource(this.components);
+            this.yazlab1_tarifDataSet11 = new yazlabProje1.yazlab1_tarifDataSet11();
+            this.tbl_TariflerTableAdapter3 = new yazlabProje1.yazlab1_tarifDataSet11TableAdapters.Tbl_TariflerTableAdapter();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.yazlab1_tarifDataSet8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblTariflerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yazlab1_tarifDataSet9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblTariflerBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yazlab1_tarifDataSet10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblTariflerBindingSource2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblTariflerBindingSource3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yazlab1_tarifDataSet11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(208, 72);
+            this.listBox1.Location = new System.Drawing.Point(26, 232);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(441, 548);
+            this.listBox1.Size = new System.Drawing.Size(188, 356);
             this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(867, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(237, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(756, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "siralama";
+            // 
+            // yazlab1_tarifDataSet8
+            // 
+            this.yazlab1_tarifDataSet8.DataSetName = "yazlab1_tarifDataSet8";
+            this.yazlab1_tarifDataSet8.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblTariflerBindingSource
+            // 
+            this.tblTariflerBindingSource.DataMember = "Tbl_Tarifler";
+            this.tblTariflerBindingSource.DataSource = this.yazlab1_tarifDataSet8;
+            // 
+            // tbl_TariflerTableAdapter
+            // 
+            this.tbl_TariflerTableAdapter.ClearBeforeFill = true;
+            // 
+            // yazlab1_tarifDataSet9
+            // 
+            this.yazlab1_tarifDataSet9.DataSetName = "yazlab1_tarifDataSet9";
+            this.yazlab1_tarifDataSet9.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblTariflerBindingSource1
+            // 
+            this.tblTariflerBindingSource1.DataMember = "Tbl_Tarifler";
+            this.tblTariflerBindingSource1.DataSource = this.yazlab1_tarifDataSet9;
+            // 
+            // tbl_TariflerTableAdapter1
+            // 
+            this.tbl_TariflerTableAdapter1.ClearBeforeFill = true;
+            // 
+            // yazlab1_tarifDataSet10
+            // 
+            this.yazlab1_tarifDataSet10.DataSetName = "yazlab1_tarifDataSet10";
+            this.yazlab1_tarifDataSet10.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblTariflerBindingSource2
+            // 
+            this.tblTariflerBindingSource2.DataMember = "Tbl_Tarifler";
+            this.tblTariflerBindingSource2.DataSource = this.yazlab1_tarifDataSet10;
+            // 
+            // tbl_TariflerTableAdapter2
+            // 
+            this.tbl_TariflerTableAdapter2.ClearBeforeFill = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.tarifAdiDataGridViewTextBoxColumn,
+            this.hazirlanmaSuresiDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tblTariflerBindingSource3;
+            this.dataGridView1.Location = new System.Drawing.Point(242, 232);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(475, 326);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // tarifAdiDataGridViewTextBoxColumn
+            // 
+            this.tarifAdiDataGridViewTextBoxColumn.DataPropertyName = "TarifAdi";
+            this.tarifAdiDataGridViewTextBoxColumn.HeaderText = "TarifAdi";
+            this.tarifAdiDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.tarifAdiDataGridViewTextBoxColumn.Name = "tarifAdiDataGridViewTextBoxColumn";
+            this.tarifAdiDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // hazirlanmaSuresiDataGridViewTextBoxColumn
+            // 
+            this.hazirlanmaSuresiDataGridViewTextBoxColumn.DataPropertyName = "HazirlanmaSuresi";
+            this.hazirlanmaSuresiDataGridViewTextBoxColumn.HeaderText = "HazirlanmaSuresi";
+            this.hazirlanmaSuresiDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.hazirlanmaSuresiDataGridViewTextBoxColumn.Name = "hazirlanmaSuresiDataGridViewTextBoxColumn";
+            this.hazirlanmaSuresiDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // tblTariflerBindingSource3
+            // 
+            this.tblTariflerBindingSource3.DataMember = "Tbl_Tarifler";
+            this.tblTariflerBindingSource3.DataSource = this.yazlab1_tarifDataSet11;
+            // 
+            // yazlab1_tarifDataSet11
+            // 
+            this.yazlab1_tarifDataSet11.DataSetName = "yazlab1_tarifDataSet11";
+            this.yazlab1_tarifDataSet11.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tbl_TariflerTableAdapter3
+            // 
+            this.tbl_TariflerTableAdapter3.ClearBeforeFill = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(745, 232);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(398, 326);
+            this.dataGridView2.TabIndex = 4;
             // 
             // siralamaformu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1180, 632);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.listBox1);
             this.Name = "siralamaformu";
             this.Text = "siralamaformu";
             this.Load += new System.EventHandler(this.siralamaformu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.yazlab1_tarifDataSet8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblTariflerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yazlab1_tarifDataSet9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblTariflerBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yazlab1_tarifDataSet10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblTariflerBindingSource2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblTariflerBindingSource3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yazlab1_tarifDataSet11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private yazlab1_tarifDataSet8 yazlab1_tarifDataSet8;
+        private System.Windows.Forms.BindingSource tblTariflerBindingSource;
+        private yazlab1_tarifDataSet8TableAdapters.Tbl_TariflerTableAdapter tbl_TariflerTableAdapter;
+        private yazlab1_tarifDataSet9 yazlab1_tarifDataSet9;
+        private System.Windows.Forms.BindingSource tblTariflerBindingSource1;
+        private yazlab1_tarifDataSet9TableAdapters.Tbl_TariflerTableAdapter tbl_TariflerTableAdapter1;
+        private yazlab1_tarifDataSet10 yazlab1_tarifDataSet10;
+        private System.Windows.Forms.BindingSource tblTariflerBindingSource2;
+        private yazlab1_tarifDataSet10TableAdapters.Tbl_TariflerTableAdapter tbl_TariflerTableAdapter2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private yazlab1_tarifDataSet11 yazlab1_tarifDataSet11;
+        private System.Windows.Forms.BindingSource tblTariflerBindingSource3;
+        private yazlab1_tarifDataSet11TableAdapters.Tbl_TariflerTableAdapter tbl_TariflerTableAdapter3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tarifAdiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hazirlanmaSuresiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
--- yazlabProje1/siralamaformu.cs
+++ yazlabProje1/siralamaformu.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace yazlabProje1
@@ -10,11 +12,95 @@ namespace yazlabProje1
         public siralamaformu()
         {
             InitializeComponent();
+
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+
         }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Sıralama türüne göre işlemleri belirle
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "Fiyata Göre Artan":
+                    SortDataGridViewByCost(true); // Artan sıralama
+                    break;
+                case "Fiyata Göre Azalan":
+                    SortDataGridViewByCost(false); // Azalan sıralama
+                    break;
+                case "Hazırlanma Süresine Göre Artan":
+                    SortDataGridViewByPreparationTime(true); // Artan sıralama
+                    break;
+                case "Hazırlanma Süresine Göre Azalan":
+                    SortDataGridViewByPreparationTime(false); // Azalan sıralama
+                    break;
+                default:
+                    MessageBox.Show("Geçersiz seçim.");
+                    break;
+            }
+        }
+
+        // Toplam maliyete göre sıralama işlemi
+        private void SortDataGridViewByCost(bool ascending)
+        {
+            var sortedRows = dataGridView2.Rows.Cast<DataGridViewRow>()
+                .Where(row => row.Cells[1].Value != null) // Maliyet hücresi null değilse
+                .OrderBy(row => ascending ? Convert.ToSingle(row.Cells[1].Value) : -Convert.ToSingle(row.Cells[1].Value))
+                .ToList();
+
+            dataGridView2.Rows.Clear();
+            foreach (var row in sortedRows)
+            {
+                dataGridView2.Rows.Add(row.Cells[0].Value, row.Cells[1].Value);
+            }
+        }
+
+        // Hazırlanma süresine göre sıralama işlemi
+        private void SortDataGridViewByPreparationTime(bool ascending)
+        {
+            string connectionString = "Data Source=DIDIM\\SQLEXPRESS;Initial Catalog=yazlab1_tarif;Integrated Security=True;";
+            string orderBy = ascending ? "ASC" : "DESC";
+
+            string query = $"SELECT * FROM Tbl_Tarifler ORDER BY HazirlanmaSuresi {orderBy}";
+
+            DataTable dataTable = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                try
+                {
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(dataTable);
+                    dataGridView1.DataSource = dataTable; // Hazırlanma süresi verilerini dataGridView1'e bağla
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Veriler yüklenirken bir hata oluştu: " + ex.Message);
+                }
+            }
+        }
+
 
         private void siralamaformu_Load(object sender, EventArgs e)
         {
+            comboBox1.Items.Add("Fiyata Göre Artan");
+            comboBox1.Items.Add("Fiyata Göre Azalan");
+            comboBox1.Items.Add("Hazırlanma Süresine Göre Artan");
+            comboBox1.Items.Add("Hazırlanma Süresine Göre Azalan");
+
+
+
+            // TODO: Bu kod satırı 'yazlab1_tarifDataSet11.Tbl_Tarifler' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.tbl_TariflerTableAdapter3.Fill(this.yazlab1_tarifDataSet11.Tbl_Tarifler);
+            // TODO: Bu kod satırı 'yazlab1_tarifDataSet10.Tbl_Tarifler' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.tbl_TariflerTableAdapter2.Fill(this.yazlab1_tarifDataSet10.Tbl_Tarifler);
+            // TODO: Bu kod satırı 'yazlab1_tarifDataSet9.Tbl_Tarifler' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.tbl_TariflerTableAdapter1.Fill(this.yazlab1_tarifDataSet9.Tbl_Tarifler);
+            dataGridView2.Columns.Add("TarifID", "Tarif Adi");
+            dataGridView2.Columns.Add("ToplamMaliyet", "Toplam Maliyet");
+
             TumTariflerinMaliyetleriniHesaplaVeListele();
+            TumTarifleriListele();
         }
         private float HesaplaToplamMaliyet(int selectedTarifID)
         {
@@ -166,6 +252,19 @@ namespace yazlabProje1
                 {
                     // ListBox'a TarifID ve maliyeti yazdır
                     listBox1.Items.Add("Tarif ID: " + tarifID + " - Toplam Maliyet: " + maliyet.ToString("0.00") + " TL");
+                    int rowIndex = dataGridView2.Rows.Add();
+
+                    DataGridViewRow newRow = dataGridView2.Rows[rowIndex];
+                    // ************************************************
+                    // ************************************************
+                    // ************************************************
+                    // newRow.Cells[0].Value = tarifID; // İlk sütuna Tarif ID'sini ekle
+                    // Tarif adını al
+                    string tarifAdi = GetTarifAdi(tarifID);
+
+                    // İlk sütuna Tarif Adı'nı ekle
+                    newRow.Cells[0].Value = tarifAdi;
+                    newRow.Cells[1].Value = maliyet; // İkinci sütuna Maliyet'i ekle
                 }
                 else
                 {
@@ -173,6 +272,81 @@ namespace yazlabProje1
                 }
             }
         }
+        private string GetTarifAdi(int tarifID)
+        {
+            string connectionString = "Data Source=DIDIM\\SQLEXPRESS;Initial Catalog=yazlab1_tarif;Integrated Security=True;";
+            string tarifAdi = "";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT TarifAdi FROM Tbl_Tarifler WHERE TarifID = @TarifID";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@TarifID", tarifID);
+
+                try
+                {
+                    connection.Open();
+                    var result = command.ExecuteScalar();
+                    if (result != null)
+                    {
+                        tarifAdi = result.ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Tarif adı alınırken bir hata oluştu: " + ex.Message);
+                }
+            }
+
+            return tarifAdi;
+        }
+
+
+
+        private void TumTarifleriListele()
+        {
+            string connectionString = "Data Source=DIDIM\\SQLEXPRESS;Initial Catalog=yazlab1_tarif;Integrated Security=True;";
+
+            // DataTable oluştur
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string queryTarifler = "SELECT * FROM Tbl_Tarifler"; // Tüm tarifleri seç
+                SqlCommand command = new SqlCommand(queryTarifler, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(dataTable); // Verileri DataTable'a doldur
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Tarifler yüklenirken bir hata oluştu: " + ex.Message);
+                    return;
+                }
+            }
+
+            // DataGridView'e DataTable'ı bağla
+            dataGridView1.DataSource = dataTable;
+        }
+
+
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            var sortedItems = listBox1.Items.Cast<string>()
+                .OrderBy(item => item)
+                .ToList();
+
+           
+        }
+
+
+
 
 
     }
